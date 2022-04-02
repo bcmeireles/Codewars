07687fed90f5a3590289a1137c995647bb9dfbc0
Bernardo Meireles <67381633+meirelesbc@users.noreply.github.com>
2022-04-02 23:45:15 +0100

    Update kyu05.py

--- Python/kyu05.py
+++ Python/kyu05.py
@@ -1,6 +1,6 @@
 """
 Calculating with Functions
-https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39
 """
 def zero(operation = None): return 0 if operation is None else operation(0)
 def one(operation = None): return 1 if operation is None else operation(1)
